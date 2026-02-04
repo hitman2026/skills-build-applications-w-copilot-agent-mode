8382ff32277c70477aa972ca1cfd7fbe8cfae93c
hitman2026 <shivendra.singh2@gds.ey.com>
2026-02-04 10:26:26 +0000

    Front end working

---
+++ octofit-tracker/frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to OctoFit Tracker Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
